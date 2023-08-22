b99c5d1e55ac0d326601d829d22300fb57cd9340
Colin McEwan <colin.mcewan@mediatek.com>
2023-08-23 03:33:39 +0800

    Revert "Add csmith-generated tests"
    This reverts commit 08535016a9efe165a2efcb9dee30c2820c1edb4b.

--- mtk_tests/lsf1_94460_mtkscf116_reduce_test.c
+++
@@ -1,9 +0,0 @@
-#include "csmith.h"
-uint16_t a[] = {3};
-int main() {
-  int b, c = 0;
-  crc32_gentab();
-  for (b = 0; b < 9; b++)
-    transparent_crc(a[b], "", c);
-  platform_main_end(crc32_context, 0);
-}
--- mtk_tests/lsf1_98881_mtkscf116_reduce_test.c
+++
@@ -1,9 +0,0 @@
-#include "csmith.h"
-static int8_t a[] = {5, 5};
-int main() {
-  int b, c = 0;
-  crc32_gentab();
-  for (b = 0; b < 9; b++)
-    transparent_crc(a[b], "", c);
-  platform_main_end(crc32_context, 0);
-}
